480902a53308bfc359752620f65dd252fdbb601b
jwolf-12 <prachijain12026@gmail.com>
2026-01-16 20:01:36 +0530

    Add main

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import bisect
+from collections import deque
+from collections import defaultdict
+import heapq
+
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lii = lambda: list(mii())
+li= lambda: list(input())
+INF = sys.maxsize
+
+def solve():
+    return
+
+for _ in range(int(input())):
+    solve()
