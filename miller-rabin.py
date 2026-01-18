a9ae4fc3d658bd652481064ee4c2c55813395ebd
jwolf-12 <prachijain12026@gmail.com>
2026-01-18 13:32:05 +0530

    Add Miller Rabin

---
+++ miller-rabin.py
@@ -0,0 +1,31 @@
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+    small_primes = [2,3,5,7,11,13,17,19,23,29]
+    for p in small_primes:
+        if n == p:
+            return True
+        if n % p == 0:
+            return n == p
+
+    # write n-1 = d * 2^s
+    d = n - 1
+    s = 0
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+
+    # bases enough for n < 2^64
+    for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
+        if a % n == 0:
+            continue
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(s - 1):
+            x = (x * x) % n
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
