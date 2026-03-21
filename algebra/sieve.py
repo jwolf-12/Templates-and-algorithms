81d1887310e6d4c5eb12d4901603351c49ef1f1f
jwolf-12 <prachijain12026@gmail.com>
2026-03-21 17:17:27 +0530

    sieve update

--- algebra/sieve.py
+++ algebra/sieve.py
@@ -10,3 +10,25 @@ def sieve(n):
 
     primes = [i for i in range(2, n + 1) if is_prime[i]]
     return primes, is_prime
+
+N = 2 * 10**5 + 5
+spf = list(range(N))
+
+def sieve():
+    for i in range(2, int(N**0.5) + 1):
+        if spf[i] == i:  # i is prime
+            for j in range(i * i, N, i):
+                if spf[j] == j:
+                    spf[j] = i
+sieve()
+
+def factorize(x):
+    res = []
+    while x > 1:
+        p = spf[x]
+        cnt = 0
+        while x % p == 0:
+            x //= p
+            cnt += 1
+        res.append((p, cnt))
+    return res
